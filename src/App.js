20d0ccb6554e5e95e1fa73e7f901f452764c12b9
GudrunStella <gsa15@hi.is>
2022-06-10 13:50:19 +0000

    fixed the value

--- src/App.js
+++ src/App.js
@@ -16,157 +16,128 @@ const App = () => {
   let [result, setResult] = useState(0);
 
 
+  const numHandler = (value) => {
+    return (e) => {
+      e.preventDefault();
 
-  const numHandler = (e) => {
-    e.preventDefault();
-
-    let value = e.target.id;
-    if (num === 0) {
-      num = Number(value)
-    } else {
       num = Number(num + value)
-    }
-    setNum(num)
+      setNum(num)
 
-    //Calculation for +
-    if (operate === '+') {
-      if (temp === 0) {
-        temp = prevNum + num
-      } else {
-        temp += num;
-
-      }
-      setTemp(temp)
-      setResult(calc + num)
-
-      //Calculation for -
-    } else if (operate === '-') {
-      if (temp === 0) {
-        temp = prevNum - num
-      } else {
-        temp -= num
-      }
-      setTemp(temp)
-      setResult(calc - num)
-
-      //Calculation for *
-    } else if (operate === '*') {
-      if (temp === 0) {
-        temp = prevNum * num
-      } else {
-        temp *= num
-      }
-      setTemp(temp)
-      setResult(calc * num)
-
-      //Calculation for /
-    } else if (operate === '/') {
-      if (temp === 0) {
-        temp = prevNum / num
-      } else {
-        temp /= num
-      }
-      setTemp(temp)
-      setResult(calc / num)
-
-      //Calculation for /
-    } else if (operate === '^') {
-      var numPow = 1;
-      for (var i = 0; i < num; i++) {
-        numPow *= prevNum;
-      }
-      setCalc(calc = numPow)
-      setResult(result = calc)
-
-
-    } else if (operate === '=') {
-      setSign(sign += ' ')
-    }
-  }
-
-
-
-  const operateHandler = (e) => {
-
-    e.preventDefault();
-
-    const value = e.target.id;
-    setPrevNum(num)
-    if (temp === 0) setTemp(prevNum)
-
-    switch (value) {
-      case '+':
-        setCalc(temp);
-        setOperate('+');
-        break;
-      case '-':
-        setCalc(temp);
-        setOperate('-')
-        break;
-      case '*':
-        setCalc(temp)
-        setOperate('*')
-        break;
-      case '/':
-        setCalc(temp)
-        setOperate('/')
-        break;
-      case '%':
+      //Calculation for +
+      if (operate === '+') {
         if (temp === 0) {
-          setTemp(num / 100)
+          temp = prevNum + num
         } else {
-          setTemp(temp /= 100)
+          temp += num;
         }
-        setCalc(temp)
-        setResult(calc)
-        setOperate('%')
-        break;
+        setResult(calc + num)
 
-      case '^':
-        setOperate('^')
-        break;
-
-      case 'sqr':
-        setCalc(calc = result)
-        var count = 0;
-        var i = 0;
-        if (calc === 0) {
-          for (i = 1; i < num / 2; i++) {
-            count += 1;
-            if (i * i === num) break;
-          }
-          setSign(sign = 'sqr(' + num + ')')
+        //Calculation for -
+      } else if (operate === '-') {
+        if (temp === 0) {
+          temp = prevNum - num
         } else {
-          for (i = 1; i < calc / 2; i++) {
-            count += 1;
-            if (i * i === calc) break;
-          }
-          setSign(sign = 'sqr(' + calc + ')')
+          temp -= num
         }
-        setTemp(temp = count);
-        setSign(sign += '=' + temp)
+        setResult(calc - num)
 
-        setOperate('sqr')
-        break;
+        //Calculation for *
+      } else if (operate === '*') {
+        if (temp === 0) {
+          temp = prevNum * num
+        } else {
+          temp *= num
+        }
+        setResult(calc * num)
 
-      case 'c':
-        setCalc(0)
-        setTemp(0)
-        setSign('')
-        setOperate('')
-        setPrevNum(0)
-        break;
-      default:
-        setSign('=' + result)
-        setOperate('=')
-    }
+        //Calculation for /
+      } else if (operate === '/') {
+        if (temp === 0) {
+          temp = prevNum / num
+        } else {
+          temp /= num
+        }
+        setResult(calc / num)
 
-    if (value === 'c') {
-      setNum(0)
-    } else {
+        //Calculation for /
+      } else if (operate === '^') {
+        var numPow = 1;
+        for (var i = 0; i < num; i++) {
+          numPow *= prevNum;
+        }
+        setCalc(calc = numPow)
+        setResult(result = calc)
+
+
+      } else if (operate === '=') {
+        setSign(sign += ' ')
+      }
+
+      setTemp(temp)
+    };
+  };
+
+
+
+  const operateHandler = (value) => {
+    return (e) => {
+      e.preventDefault();
+      setPrevNum(num)
+      if (temp === 0) setTemp(prevNum)
+
+      switch (value) {
+
+        case '%':
+
+          if (temp === 0) {
+            setTemp(num / 100)
+          } else {
+            setTemp(temp /= 100)
+          }
+          setResult(calc)
+          break;
+
+        case 'sqr':
+          setCalc(calc = result)
+          var count = 0;
+          var i = 0;
+          if (calc === 0) {
+            for (i = 1; i < num / 2; i++) {
+              count += 1;
+              if (i * i === num) break;
+            }
+            setSign(sign = 'sqr(' + num + ')')
+          } else {
+            for (i = 1; i < calc / 2; i++) {
+              count += 1;
+              if (i * i === calc) break;
+            }
+            setSign(sign = 'sqr(' + calc + ')')
+          }
+          setTemp(temp = count);
+          setSign(sign += '=' + temp)
+          break;
+
+        case 'c':
+          setNum(0)
+          setCalc(0)
+          setTemp(0)
+          setSign('')
+          setOperate('')
+          setPrevNum(0)
+          break;
+
+        default:
+          setSign('=' + result)
+
+      }
+
       if (value !== '=' && value !== 'sqr') setSign(sign += num + value)
+      setCalc(temp)
+      setOperate(value)
       setNum('')
     }
+
   }
 
 
@@ -176,26 +147,26 @@ const App = () => {
         <textarea className='window' type="number" value={num} placeholder={sign}></textarea>
       </div>
       <div className='buttons-container'>
-        <button className='top-button' id='%' onClick={operateHandler}>%</button>
-        <button className='top-button' id='^' onClick={operateHandler}>x<sup>n</sup></button>
-        <button className='top-button' id='sqr' onClick={operateHandler}>&#8730;</button>
-        <button className='clear-button' id='c' onClick={operateHandler}>C</button>
-        <button className='operate-button' id='/' onClick={operateHandler}>/</button>
-        <button className='tag-button' id='9' onClick={numHandler} value='9'>9</button>
-        <button className='tag-button' id='8' onClick={numHandler}>8</button>
-        <button className='tag-button' id='7' onClick={numHandler}>7</button>
-        <button className='operate-button' id='*' onClick={operateHandler}>*</button>
-        <button className='tag-button' id='6' onClick={numHandler}>6</button>
-        <button className='tag-button' id='5' onClick={numHandler}>5</button>
-        <button className='tag-button' id='4' onClick={numHandler}>4</button>
-        <button className='operate-button' id='-' onClick={operateHandler}>-</button>
-        <button className='tag-button' id='3' onClick={numHandler}>3</button>
-        <button className='tag-button' id='2' onClick={numHandler}>2</button>
-        <button className='tag-button' id='1' onClick={numHandler}>1</button>
-        <button className='equal-button' id='=' onClick={operateHandler}>=</button>
-        <button className='operate-button' id='+' onClick={operateHandler}>+</button>
-        <button className='tag-button' id='.'>.</button>
-        <button className='tag-button' id='0' onClick={numHandler}>0</button>
+        <button className='top-button' onClick={operateHandler('%')}>%</button>
+        <button className='top-button' onClick={operateHandler('^')}>x<sup>n</sup></button>
+        <button className='top-button' onClick={operateHandler('sqr')}>&#8730;</button>
+        <button className='clear-button' onClick={operateHandler('c')}>C</button>
+        <button className='operate-button' onClick={operateHandler('/')}>/</button>
+        <button className='tag-button' onClick={numHandler(9)}>9</button>
+        <button className='tag-button' onClick={numHandler(8)}>8</button>
+        <button className='tag-button' onClick={numHandler(7)}>7</button>
+        <button className='operate-button' onClick={operateHandler('*')}>*</button>
+        <button className='tag-button' onClick={numHandler(6)}>6</button>
+        <button className='tag-button' onClick={numHandler(5)}>5</button>
+        <button className='tag-button' onClick={numHandler(4)}>4</button>
+        <button className='operate-button' onClick={operateHandler('-')}>-</button>
+        <button className='tag-button' onClick={numHandler(3)}>3</button>
+        <button className='tag-button' onClick={numHandler(2)}>2</button>
+        <button className='tag-button' onClick={numHandler(1)}>1</button>
+        <button className='equal-button' onClick={operateHandler('=')}>=</button>
+        <button className='operate-button' onClick={operateHandler('+')}>+</button>
+        <button className='tag-button' >.</button>
+        <button className='tag-button' onClick={numHandler(0)}>0</button>
       </div>
     </div>
   );
